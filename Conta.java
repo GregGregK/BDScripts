c730842974906a12d0110329ce59530afc91b41f
Gregori R. M <69245767+GregGregK@users.noreply.github.com>
2021-04-29 08:20:27 -0300

    Add files via upload

---
+++ Conta.java
@@ -0,0 +1,50 @@
+package resolucaolistaexercicio;
+
+public class Conta {
+    private int numero;
+    private String nome;
+    private double saldo;
+    
+    public Conta(int numero, String nome){
+        this.numero = numero;
+        this.nome = nome;
+        saldo = 0.0;
+    }
+    public Conta(int numero, String nome, double saldo){
+        this.numero = numero;
+        this.nome = nome;
+        this.saldo = saldo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    
+    //déposito
+    public void deposito(double valor){
+        this.saldo += valor;
+    }
+    
+    //saque
+    public void saque(double valor){
+        this.saldo = this.saldo -5.0 - valor;
+        //this.saldo -= valor (-5);
+    }
+    
+    public void imprimir(){
+        System.out.println("Conta: "+numero
+                +", Titular: "+nome
+                +", Saldo: "+saldo+"\n");
+    }
+    
+    
+}
